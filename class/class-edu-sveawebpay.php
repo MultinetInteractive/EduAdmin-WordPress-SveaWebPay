2cfccac9e5b67bc6dbf850e207db41d329da4f9c
Chris Gårdenberg <chga@multinet.se>
2017-04-13 13:15:49 +0200

    Added action to handle bookings from `eduadmin-processbooking` Currently uses `print_r` to show booking info.

--- class/class-edu-sveawebpay.php
+++ class/class-edu-sveawebpay.php
@@ -14,28 +14,47 @@ class EDU_SveaWebPay extends EDU_Integration {
 		$this->displayName = __('Svea Webpay', 'eduadmin-sveawebpay');
 		$this->description = '';
 
+		$this->init_form_fields();
 		$this->init_settings();
+
+		add_action( 'eduadmin-processbooking', array( $this, 'process_booking' ) );
 	}
 
-	public function init_settings() {
+	public function init_form_fields() {
 		$this->setting_fields =	array(
 			'enabled' => array(
-				'title'			=> __('Enabled', 'eduadmin-sveawebpay'),
-				'type'			=> 'checkbox',
-				'description'	=> __('Enables/Disabled the integration with Svea WebPay', 'eduadmin-sveawebpay'),
+				'title'       => __( 'Enabled', 'eduadmin-sveawebpay' ),
+				'type'        => 'checkbox',
+				'description' => __( 'Enables/Disables the integration with Svea WebPay', 'eduadmin-sveawebpay' ),
+				'default'     => 'no',
+			),
+			'testrun' => array(
+				'title'          => __( 'Sandbox mode', 'eduadmin-sveawebpay' ),
+				'type'           => 'checkbox',
+				'description'    => __( 'Activate sandbox mode', 'eduadmin-sveawebpay' ),
 				'default'		=> 'no'
 			),
 			'merchant_key' => array(
-				'title'			=> __('Merchant key', 'eduadmin-sveawebpay'),
-				'type'			=> 'text',
-				'description'	=> __('Please enter your merchant key from Svea WebPay.', 'eduadmin-sveawebpay')
+				'title'       => __( 'Merchant key', 'eduadmin-sveawebpay' ),
+				'type'        => 'text',
+				'description' => __( 'Please enter your merchant key from Svea WebPay.', 'eduadmin-sveawebpay' ),
+				'placeholder' => __( 'Merchant key', 'eduadmin-sveawebpay' ),
 			),
 			'merchant_secret' => array(
-				'title'			=> __('Merchant secret', 'eduadmin-sveawebpay'),
-				'type'			=> 'password',
-				'description'	=> ''
+				'title'       => __( 'Merchant secret', 'eduadmin-sveawebpay' ),
+				'type'        => 'password',
+				'description' => __( 'Please enter your merchant secret from Svea WebPay', 'eduadmin-sveawebpay' ),
+				'placeholder' => __( 'Merchant secret', 'eduadmin-sveawebpay' ),
 			)
 		);
+	}
+
+	/**
+	 * @param $bookingInfo EduAdminBookingInfo
+	 */
+	public function process_booking( $bookingInfo = null ) {
+		echo '<pre>' . print_r( $this, true ) . '</pre>';
+		echo '<pre>' . print_r( $bookingInfo, true ) . '</pre>';
 	}
 }
 
