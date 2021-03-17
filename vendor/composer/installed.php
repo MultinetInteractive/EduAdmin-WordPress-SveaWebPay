da441c5f1c3aa4bbc938608861beb35e18acb6c7
Chris Gårdenberg <chga@multinet.se>
2021-03-17 13:19:58 +0100

    chore: Updating dependencies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,66 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5bdb2bc0180c5c2aa7d2d3e3eaf36fb74d0bc0bc',
+    'name' => 'multinetinteractive/eduadmin-wordpress-sveawebpay',
+  ),
+  'versions' => 
+  array (
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v1.10.0',
+      'version' => '1.10.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1a0357fccad9d1cc1ea0c9a05b8847fbccccb78d',
+    ),
+    'multinetinteractive/eduadmin-wordpress-sveawebpay' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5bdb2bc0180c5c2aa7d2d3e3eaf36fb74d0bc0bc',
+    ),
+    'roundcube/plugin-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'shama/baton' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'sveaekonomi/checkout' => 
+    array (
+      'pretty_version' => '1.2.3',
+      'version' => '1.2.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '12fa71b0d3a38685ead53d3c97f83f3bc59c3b62',
+    ),
+    'sveaekonomi/webpay' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '9630ffae3c85368038a806a4b90e184884f4042e',
+    ),
+  ),
+);
