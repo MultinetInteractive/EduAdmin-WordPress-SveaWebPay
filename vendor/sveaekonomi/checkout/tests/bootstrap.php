d2e04db24ec0002685123da1d2c44737560e7f5a
Chris Gårdenberg <chga@multinet.se>
2021-03-17 13:47:57 +0100

    chore: Remove files we don't want/need

--- vendor/sveaekonomi/checkout/tests/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$loader = include dirname(__DIR__) . '/vendor/autoload.php';
-$loader->addPsr4('Svea\\Checkout\\Tests\\', __DIR__ . '/');
-date_default_timezone_set('UTC');
--- vendor/sveaekonomi/webpay/test/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$loader = include dirname(__DIR__) . '/vendor/autoload.php';
-
-$loader->addPsr4('Svea\\WebPay\\Test\\', __DIR__ . '/');
-date_default_timezone_set('UTC');
