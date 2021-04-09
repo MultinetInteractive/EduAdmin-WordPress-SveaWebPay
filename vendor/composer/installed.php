dc84f42cf71eb7b4a113ed99d9929dadcdd6932a
Chris Gårdenberg <chga@multinet.se>
2021-04-09 12:52:29 +0200

    chore: Re-adding all files for svea and composer.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,66 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b9d3a55c1d420f6d1cf9bc0eb19952b0cb287361',
+    'name' => 'multinetinteractive/eduadmin-wordpress-sveawebpay',
+  ),
+  'versions' => 
+  array (
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v1.10.0',
+      'version' => '1.10.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1a0357fccad9d1cc1ea0c9a05b8847fbccccb78d',
+    ),
+    'multinetinteractive/eduadmin-wordpress-sveawebpay' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b9d3a55c1d420f6d1cf9bc0eb19952b0cb287361',
+    ),
+    'roundcube/plugin-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'shama/baton' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'sveaekonomi/checkout' => 
+    array (
+      'pretty_version' => '1.3.0',
+      'version' => '1.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c5d6450a6b8b56637b52c6dac06625dc584974cf',
+    ),
+    'sveaekonomi/webpay' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '9630ffae3c85368038a806a4b90e184884f4042e',
+    ),
+  ),
+);
