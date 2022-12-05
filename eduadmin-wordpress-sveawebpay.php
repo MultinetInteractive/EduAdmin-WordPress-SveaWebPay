7a8b097e6953426c14b23ebd5f21b78c64cca214
Chris Gårdenberg <chga@multinet.se>
2022-12-05 09:19:14 +0100

    chore: bump version

--- eduadmin-wordpress-sveawebpay.php
+++ eduadmin-wordpress-sveawebpay.php
@@ -6,7 +6,7 @@ defined( 'ABSPATH' ) or die( 'This plugin must be run within the scope of WordPr
  * Plugin URI:	http://www.eduadmin.se
  * Description:	EduAdmin plugin to allow visitors to book courses at your website
  * Tags:	booking, participants, courses, events, eduadmin, lega online
- * Version:	3.0.0
+ * Version:	3.0.1
  * GitHub Plugin URI: multinetinteractive/eduadmin-wordpress-sveawebpay
  * GitHub Plugin URI: https://github.com/multinetinteractive/eduadmin-wordpress-sveawebpay
  * Requires at least: 5.0
